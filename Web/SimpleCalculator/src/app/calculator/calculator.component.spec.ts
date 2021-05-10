fbc6545ac8e667dfd8f42c7234dcd0c49adfaa5e
matthiashazeleger <matthiashaze@hotmail.com>
2021-05-10 15:56:01 +0200

    Added tests for the front end

--- Web/SimpleCalculator/src/app/calculator/calculator.component.spec.ts
+++ Web/SimpleCalculator/src/app/calculator/calculator.component.spec.ts
@@ -1,16 +1,29 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { CalculatorService } from '../services/calculator.service';
 
 import { CalculatorComponent } from './calculator.component';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
   let fixture: ComponentFixture<CalculatorComponent>;
+  let mockCalculatorService: CalculatorService;
 
   beforeEach(async () => {
+
+    mockCalculatorService = jasmine.createSpyObj('calculatorService', {
+      deleteHistory: of(),
+      calculateResult: of(),
+      getCalculationHistory: of()
+    });
+
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      declarations: [ CalculatorComponent ]
+      declarations: [ CalculatorComponent ],
+      providers: [
+        { provide: CalculatorService, useValue: mockCalculatorService }
+      ]
     })
     .compileComponents();
   });
@@ -23,5 +36,26 @@ describe('CalculatorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the service when the = button is clicked', () => {
+    component.ScreenText = "5 + 5";
+    component.calculate();
+    
+    expect(mockCalculatorService.calculateResult).toHaveBeenCalledWith("5 + 5");
+  });
+
+  it('should remove all info when clear all is clicked', () => {
+    component.clearAll();
+
+    expect(mockCalculatorService.deleteHistory).toHaveBeenCalled();
+  });
+
+  it('should clear the line when clear is clicked', () => {
+    component.clear();
+
+    expect(component.ScreenText).toEqual("");
+    expect(component.Error).toEqual("");
+    expect(component.ShowStepsTaken).toBe(false);
   });
 });
--- Web/SimpleCalculator/src/app/services/calculator.service.spec.ts
+++ Web/SimpleCalculator/src/app/services/calculator.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { CalculatorService } from './calculator.service';
+import { environment } from 'src/environments/environment';
 
 describe('CalculatorService', () => {
   let service: CalculatorService;
@@ -17,5 +18,41 @@ describe('CalculatorService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be able to delete the history', () => {
+    service.deleteHistory().subscribe();
+
+    const http = httpMock.expectOne(`${environment.SimpleCalcularAPI_url}`);
+    expect(http.request.method).toBe('DELETE');
+    expect(http.request.body).toBeNull();
+  });
+
+  it('should be able to get the calculation history', () => {
+    const response = [{}];
+
+    service.getCalculationHistory().subscribe(res => {
+      expect(res).toBe(response)
+    });
+
+    const http = httpMock.expectOne(`${environment.SimpleCalcularAPI_url}/all`);
+    expect(http.request.method).toBe('GET');
+    expect(http.request.body).toBeNull();
+    http.flush(response);
+  });
+
+  it('should be able to get the calculation result', () => {
+    const response = "25";
+    const calculationString = "5 + 5";
+    const mockCalculationResult = "5 + 5";
+
+    service.calculateResult(calculationString).subscribe(res => {
+      expect(res).toEqual(response)
+    });
+
+    const http = httpMock.expectOne(`${environment.SimpleCalcularAPI_url}`);
+    expect(http.request.method).toBe('POST');
+    expect(http.request.body).toEqual(mockCalculationResult);
+    http.flush(response);
   });
 });
