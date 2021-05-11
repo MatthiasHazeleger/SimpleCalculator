111e0311dc14ad58758f1f5875a87eaca19b41f1
Matthias Hazeleger <31656868+MatthiasHazeleger@users.noreply.github.com>
2021-05-11 18:43:41 +0200

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,5 +1,5 @@
 start cmd /k "cd %~dp0\SimpleCalculator & mvnw.cmd clean install & mvnw spring-boot:run"
 
-start cmd /k "cd %~dp0\Web\SimpleCalculator & npm i & npm http-server dist/SimpleCalculator" 
+start cmd /k "cd %~dp0\Web\SimpleCalculator & npm i & http-server dist/SimpleCalculator" 
 
 start "" http://localhost:8080
